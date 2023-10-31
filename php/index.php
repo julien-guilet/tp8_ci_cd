3373ecf9862db4893091d8cc8fe8fcb543be08af
max-wacquier <max.wacquier@gmail.com>
2023-10-31 14:55:58 +0100

    docker compose for php app and bdd

---
+++ php/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8"/>
+    <title>Liste</title>
+</head>
+<body>
+   <h1>Liste</h1>
+   <table>
+    <tbody>
+   <?php
+    echo '<tr><td>ID</td><td>NOM</td></tr>';
+    $pdo = null;
+    try {
+        echo 'Mon nom d\'utilisateur est ' . $_SERVER['MYSQL_DATABASE'] . ' !';
+        $pdo = new PDO("mysql:host=db;dbname=".$_ENV['BDD_DATABASE']."", $_ENV['BDD_USER'], $_ENV['BDD_PASSWORD']);
+    } catch (Exception $e) {
+        echo $e;
+    }
+    $res = array();
+    if (isset($pdo)) {
+        $stmt = $pdo->query("SELECT * FROM Data ORDER BY nom");
+        foreach ($stmt->fetchAll(PDO::FETCH_ASSOC) as $row)
+            echo '<tr><td>', $row['id'], '</td><td>', $row['nom'], '</td></tr>';
+    } else {
+        echo '<tr><td>0</td><td>Aucun</td></tr>';
+    }
+   ?>
+     </tbody>
+    </table>
+</body> 
+</html>
---
+++ sql/InitDB.sql
@@ -0,0 +1,12 @@
+CREATE TABLE Data (
+     id INTEGER NOT NULL AUTO_INCREMENT,
+     nom varchar(64) NOT NULL,
+     PRIMARY KEY (id)
+) CHARSET='utf8' COMMENT='Liste';
+
+-- Contenu de la table 
+
+INSERT INTO Data (nom) 
+VALUES ('Albert C'),
+       ('Issac A'),
+       ('Charles D');
